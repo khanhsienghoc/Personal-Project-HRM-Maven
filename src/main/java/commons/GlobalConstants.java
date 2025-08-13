da6cc4e98031db173df743e680bd920ff97d0207
Khanh <khanhhhh245@gmail.com>
2025-08-13 11:36:54 +0700

    fix: update the upload files path

--- src/main/java/commons/GlobalConstants.java
+++ src/main/java/commons/GlobalConstants.java
@@ -4,7 +4,7 @@ import java.io.File;
 public class GlobalConstants {
     public static EnvironmentList ENV;
     public static final String PROJECT_PATH = System.getProperty("user.dir");
-    public static final String UPLOAD_FILE= PROJECT_PATH + File.separator +"uploadFiles" + File.separator;
+    public static final String UPLOAD_FILE= PROJECT_PATH + File.separator +"src" + File.separator +"main"+ File.separator +"resources" + File.separator +"uploadFiles" +File.separator;
     public static final long SHORT_TIMEOUT = 5;
     public static final long LONG_TIMEOUT = 30;
     public static final String REQUIRED_ERROR_MESSAGE = "Required";
